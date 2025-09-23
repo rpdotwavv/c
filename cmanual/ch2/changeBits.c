e1dbb63683f6735bd93c0986ae3e3f38aa2dfae9
rp.wavv <120764319+rpdotwavv@users.noreply.github.com>
2025-09-23 13:29:59 -0500

    Add files via upload

--- cmanual/ch2/changeBits.c
+++ cmanual/ch2/changeBits.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-unsigned setBits(unsigned x, int p, int n, int y)
+unsigned getBits(unsigned x, char p, char n, char y)
 {
-	unsigned int maskL, maskR, maskT, xMasked, z;
+	unsigned char maskL, maskR, maskT, xMasked, z;
 
-	maskL = ~(~0 >> (p + 1));
-	maskR = (~0 >> (p + 1 - n));
+	maskL = (~0 << (p + 1));
+	maskR = ~(~0 << (p + 1 - n));
 	maskT = maskL | maskR;
 
 	xMasked = x & maskT;
 
 	z = y & (~(~0 << n));
+
+	
+	printf("Left mask = %u\n", maskL);
+	printf("Right mask = %u\n", maskR);
+	printf("Mask sum = %u\n", maskT);
+	printf("y variable value = %u\n", z);
 
 	return xMasked | (z << (p-1));
 
@@ -21,9 +27,9 @@ unsigned setBits(unsigned x, int p, int n, int y)
 int main()
 {
 	unsigned bits = 135;
-	int p = 5, n = 2, y = 3;
+	char p = 5, n = 2, y = 3;
 
-	printf("New byte = %u\n", setBits(bits, p, n, y));
+	printf("New byte = %u\n", getBits(bits, p, n, y));
 
 	return 0;
 
