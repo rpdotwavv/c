d012561f17dec7e88fd78323413099f3a3101b2b
rp.wavv <120764319+rpdotwavv@users.noreply.github.com>
2025-10-17 16:02:50 -0500

    Update toLowerVSasm.c

--- sandbox/string/toLowerVSasm.c
+++ sandbox/string/toLowerVSasm.c
@@ -1,3 +1,6 @@
+// my simple toLower program compared to x86-64 clang 21.1.0 asm via godbolt.org
+//
+//
 int main(){                                 // push    rbp
                                             //  mov     rbp, rsp
                                             //  mov     dword ptr [rbp - 4], 0
@@ -7,7 +10,7 @@ int main(){                                 // push    rbp
   char newChar;
   if (ogChar < 90){                         //  movsx   eax, byte ptr [rbp - 5]
                                             //  cmp     eax, 90
-                                            // jge     .LBB0_2
+                                            //  jge     .LBB0_2
     return newChar = ogChar + 32;           //  movsx   eax, byte ptr [rbp - 5]
                                             //  add     eax, 32
                                             //  mov     byte ptr [rbp - 6], al
@@ -19,4 +22,5 @@ int main(){                                 // push    rbp
 }            // .LBB0_3                     //  mov     eax, dword ptr [rbp - 4]
                                             //  pop     rbp
 
-                                            //   ret
+                                            //  ret
+
