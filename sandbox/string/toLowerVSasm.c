c856e09da2e670f03b936486f11237eb3af6f6ad
rp.wavv <120764319+rpdotwavv@users.noreply.github.com>
2025-10-17 15:59:10 -0500

    Add files via upload

---
+++ sandbox/string/toLowerVSasm.c
@@ -0,0 +1,21 @@
+int main(){                                 // push    rbp
+                                            //  mov     rbp, rsp
+                                            //  mov     dword ptr [rbp - 4], 0
+
+
+  char ogChar = 'H';                        //  mov     byte ptr [rbp - 5], 72
+  char newChar;
+  if (ogChar < 90){                         //  movsx   eax, byte ptr [rbp - 5]
+                                            //  cmp     eax, 90
+                                            // jge     .LBB0_2
+    return newChar = ogChar + 32;           //  movsx   eax, byte ptr [rbp - 5]
+                                            //  add     eax, 32
+                                            //  mov     byte ptr [rbp - 6], al
+                                            //  movsx   eax, al
+                                            //  mov     dword ptr [rbp - 4], eax
+                                            //  jmp     .LBB0_3
+  }
+  return 0;                                 //  mov     dword ptr [rbp - 4], 0
+}                                           //  mov     eax, dword ptr [rbp - 4]
+                                            //  pop     rbp
+                                            //   ret
